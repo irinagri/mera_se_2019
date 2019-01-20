2fbe9a641f46de2a455466cb5b7826a2b7c30ada
RomazanovaGU <romazanovagu@gmail.com>
2019-01-20 22:58:50 +0300

    [task 1] Updated text

--- _romazanova/1/ReverseListofArgs.java
+++ _romazanova/1/ReverseListofArgs.java
@@ -3,7 +3,7 @@ package _romazanova.1;
 public class ReverseListofArgs {
 
 	public static void main(String[] args) {
-		System.out.print("Command line args: ");
+		System.out.print("Command line args in reverse order: ");
 		for (int i = args.length-1; i >=0; i--) {
 			System.out.print( args [i] + " ");
 		}
